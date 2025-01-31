186d8e354bbab21fcee144a2aa85c49775b681cb
Romer Flores <romer141016@gmail.com>
2025-01-30 22:48:33 -0400

    correcion path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     build: {
+//         outDir: "../Versiones/version0_1_2" // Cambia "ruta-personalizada" por la carpeta que desees
+//     },
+//     base: "/Calculadora/"
+
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,8 +18,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: "../Versiones/version0_1_2" // Cambia "ruta-personalizada" por la carpeta que desees
+        sourcemap: true,
     },
-    base: "/Calculadora/"
-
+    base: '/',
 })
