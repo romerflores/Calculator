9284d394fd41bc538917428fc8d6d6f591f48eb4
Romer Flores <romer141016@gmail.com>
2025-02-10 19:01:50 -0400

    Correciones Home

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -5,14 +5,15 @@ interface titleProps {
     textColor?: string,
     textSize?: string,
     textAlign_?: "left" | "right" | "center" | "justify" | "start" | "end";
+    textBorder?:string
 }
 
 
 
 function Title(props: titleProps) {
-    const { textTitle, textColor = "Black", textSize = "10", textAlign_ = "center" } = props
+    const { textTitle, textColor = "Black", textSize = "10", textAlign_ = "center" ,textBorder="solid 0px withe"} = props
     return (
-        <h2 className="title" style={{ fontSize: `${textSize}px`, color: textColor, textAlign:textAlign_ as React.CSSProperties["textAlign"]}}>
+        <h2 className="title" style={{ fontSize: `${textSize}px`, color: textColor, textAlign:textAlign_ as React.CSSProperties["textAlign"],WebkitTextStroke:textBorder}}>
             {textTitle}
         </h2>
     )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,10 +11,10 @@ function Home() {
             <section className="homepage-container">    
 
                 <div className="card-home">
-                    <Title textAlign_="end" textTitle="Bienvenido, a la Pagina de Romer" textColor="var(--withe)" textSize="50"></Title>    
+                    <Title textAlign_="end" textTitle="Bienvenido, a la Pagina de Romer" textColor="var(--withe)" textSize="50" textBorder="var(--wine) 0.5px"></Title>    
                 </div>
                 <div className="card-home">
-                    <Title textAlign_="end" textTitle="Multiples herramientas de calculos matematicos y criptografia" textColor="var(--red)" textSize="25"></Title>
+                    <Title textAlign_="end" textTitle="Multiples herramientas de calculos matematicos y criptografia" textColor="var(--red)" textSize="25" textBorder="var(--white) 0.5px"></Title>
                 </div>
                 
 
