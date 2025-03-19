3c6de0db2469f26b5fecdd7a08f612545c5fc1fc
Romer Flores <romer141016@gmail.com>
2025-03-18 21:24:14 -0400

    Adiccion del local storage en TIC

--- src/pages/aprende/Tic/Tic.tsx
+++ src/pages/aprende/Tic/Tic.tsx
@@ -6,7 +6,7 @@ import Square from "../../../components/square/Square";
 
 import confetti from "canvas-confetti"
 
-import {TURNS,WINNER_STATES } from "../../../utils/constants/gamesConstants";
+import { TURNS, WINNER_STATES } from "../../../utils/constants/gamesConstants";
 
 import { checkWinner } from "../../../utils/validates";
 import WinnerModal from "../../../components/winnerModal/WinnerModal";
@@ -17,13 +17,23 @@ import WinnerModal from "../../../components/winnerModal/WinnerModal";
 
 
 function Tic() {
-    const [board, setBoard] = useState(
-        Array(9).fill("")
+    const [board, setBoard] = useState(() => { ///simpre debemos hacer esto,si hacemos afuera, se carga siempre que se renderize, pero con lo siguiente evaluamos 
+
+        const boardFromStorage = window.localStorage.getItem("board");
+
+        return (boardFromStorage)?JSON.parse(boardFromStorage):Array(9).fill("")
+    }
     );
 
-    const [turn, setTurn] = useState(TURNS.X)
+    const [turn, setTurn] = useState(()=>{
+        const turnFromStorage=window.localStorage.getItem("turn");
+        return turnFromStorage?JSON.parse(turnFromStorage):TURNS.X
+    })
 
-    const [winner, setWinner] = useState(WINNER_STATES.NO_WINNER);
+    const [winner, setWinner] = useState(()=>{
+        const winnerFromStorage=window.localStorage.getItem("winner")
+        return winnerFromStorage?JSON.parse(winnerFromStorage):WINNER_STATES.NO_WINNER;
+    });
 
 
 
@@ -40,7 +50,16 @@ function Tic() {
             const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
             setTurn(newTurn);
 
+
+            //Aca guardaremos en local storage
+            window.localStorage.setItem('board', JSON.stringify(newBoard))
+            window.localStorage.setItem('turn', JSON.stringify(newTurn))
+            
+            
+            
+            
             const currtWinner = checkWinner(newBoard);
+            window.localStorage.setItem('winner', JSON.stringify(currtWinner))
 
             if (currtWinner != WINNER_STATES.NO_WINNER) {
                 setWinner(currtWinner);
@@ -68,20 +87,20 @@ function Tic() {
             />
             <section className="game">
                 {
-                    board.map((_, index) => {
+                    board.map((element:string, index:number) => {
                         return (
                             <Square
                                 key={index}
                                 index={index}
                                 updateBoard={updateBoard}
                             >
-                                {board[index]}
+                                {element}
                             </Square>
                         )
                     })
                 }
             </section>
-            <Title textTitle="Turno:" textColor="var(--white)" textSize="20"/>
+            <Title textTitle="Turno:" textColor="var(--white)" textSize="20" />
             <section className="turn">
                 <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
                 <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
